1f3444f1440d5bbfaaa2d9c89bcfe87d483eae3f
OIITuMuCT <vataman@live.ru>
2024-12-11 19:42:39 +0300

    add models in admin panel

--- employee_learning/admin.py
+++ employee_learning/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Employee
+from .models import Division
+
 # Register your models here.
+admin.site.register(Employee)
+admin.site.register(Division)
--- employee_learning/models.py
+++ employee_learning/models.py
@@ -4,7 +4,11 @@ from django.db import models
 class Employee(models.Model):
     name = models.CharField(max_length=25)
 
+    def __str__(self):
+        return f'{self.id}: {self.name}'
 
 class Division(models.Model):
     div_name = models.CharField(max_length=25)
-    
+
+    def __str__(self):
+        return self.div_name
