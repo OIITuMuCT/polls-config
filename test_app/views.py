76ce7b50d193c754f9d709049cd73e17f09bfb01
OIITuMuCT <vataman@live.ru>
2024-12-11 19:08:05 +0300

    test class and func

--- test_app/views.py
+++ test_app/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
 def function_view_test(request):
-    return HttpResponse("<h1>Hello World! Class-based View Test</h1>")
+    return HttpResponse("<h1>Hello world! Function-based View Test</h1>")
 
 
 
-example of class 
-class ClassName(ParentClassName):
-    template_name = 'HTML template file name'
-    model = ModelName
-    # other logic in requred
+class ClassViewTest(TemplateView):
+    template_name = 'test.html'
