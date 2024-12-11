70f04909c22fddb242bb332c1f15c9d40d85b437
OIITuMuCT <vataman@live.ru>
2024-12-11 18:58:39 +0300

    example class template

--- test_app/views.py
+++ test_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def function_view_test(request):
+    return HttpResponse("<h1>Hello World! Class-based View Test</h1>")
+
+
+
+example of class 
+class ClassName(ParentClassName):
+    template_name = 'HTML template file name'
+    model = ModelName
+    # other logic in requred
