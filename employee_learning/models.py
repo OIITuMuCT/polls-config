f10f5b585ebf3edf7e9af003c7ba7aa05eb66cb6
OIITuMuCT <vataman@live.ru>
2024-12-11 19:47:31 +0300

    add field in_scope in division model

--- employee_learning/models.py
+++ employee_learning/models.py
@@ -9,6 +9,7 @@ class Employee(models.Model):
 
 class Division(models.Model):
     div_name = models.CharField(max_length=25)
+    in_scope = models.BooleanField(help_text='If the division is target for the learning program, check this filed.')
 
     def __str__(self):
         return self.div_name
