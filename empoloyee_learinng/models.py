33218822077549db4930733f32fc0d0620d4dba1
OIITuMuCT <vataman@live.ru>
2024-12-11 19:24:22 +0300

    model template

--- empoloyee_learinng/models.py
+++ empoloyee_learinng/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class ModelName(models.Model):
+    field_name = models.FieldType(option)
+    
+# example:
+
+class PetModel(models.Model):
+    pet_name = models.CharField(max_length=20)
