f32e513dfac1a56036cba6e73501fec5e72c7304
OIITuMuCT <vataman@live.ru>
2024-12-12 19:40:05 +0300

    reg_date

--- employee_learning/models.py
+++ employee_learning/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class Employee(models.Model):
     PRIORITIES = [('H', 'High'), ('M', 'Medium'), ('L', 'Low'),]
     name = models.CharField(max_length=25, verbose_name="Employee Name")
     priority = models.CharField(max_length=1, verbose_name='Learning Priorities', choices=PRIORITIES, default='M')
-    
+    reg_date = models.DateField(default=datetime.date.today, verbose_name="Data Registration Date")
     def __str__(self):
         return f'{self.id}: {self.name}'
 
