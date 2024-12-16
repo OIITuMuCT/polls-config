699cbddd3201eb350172850270880a0b4e1b270b
OIITuMuCT <vataman@live.ru>
2024-12-16 21:48:08 +0300

    Add Detail page

--- employee_learning/urls.py
+++ employee_learning/urls.py
@@ -3,7 +3,7 @@ from .views import CourseList, CourseDetail, CourseCreate, CourseUpdate, CourseD
 
 urlpatterns = [
     path('course-list/', CourseList.as_view(), name='course_list'),
-    path('course-detail/', CourseDetail.as_view(), name='course_detail'),
+    path('course-detail/<int:pk>/', CourseDetail.as_view(), name='course_detail'),
     path('course-create/', CourseCreate.as_view(), name='course_create'),
     path('course-update/<int:pk>/', CourseUpdate.as_view(), name='course_update'),
     path('course-delete/<int:pk>/', CourseDelete.as_view(), name='course_delete'),
