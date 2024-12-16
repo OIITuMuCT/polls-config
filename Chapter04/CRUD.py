e953cfb8fa1bbeff012ef128dc020cabfede6a86
OIITuMuCT <vataman@live.ru>
2024-12-16 18:41:25 +0300

    Chapter04-1

---
+++ Chapter04/CRUD.py
@@ -0,0 +1,14 @@
+# for list page
+# for Detail page
+# for Create page
+# for Update page
+# for Delete page 
+# add five urls patterns each view
+# add five Views using Generic Views
+# create 5 template
+# list.html
+# detail.html
+# create.html
+# update.html
+# delite.html
+
