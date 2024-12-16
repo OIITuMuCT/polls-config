8b1de79f5b353678cf57d2a52b83ee945a294961
OIITuMuCT <vataman@live.ru>
2024-12-16 19:12:06 +0300

    Add CRUD for Course

--- employee_learning/views.py
+++ employee_learning/views.py
@@ -1,14 +1,32 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import LearningCourse
+from django.urls import reverse_lazy
 
-# Create your views here.
-from django.views.generic import ListView
-from .models import MyModel
 
-class MyView(ListView):
-    template_name = 'list.html'
-    model = MyModel
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['now'] = timezone.now()
-        return context
+class CourseList(ListView):
+    template_name = 'employee_learning/course_list.html'
+    model = LearningCourse
+    context_object_name = 'course_object_list'
+
+class CourseDetail(DetailView):
+    template_name = 'employee_learning/course_detail.html'
+    model = LearningCourse
+    context_object_name = 'course_object'
+
+class CourseCreate(CreateView):
+    model = LearningCourse
+    template_name = 'employee_learning/course_create.html'
+    fields = ('title', 'level', 'employee')
+    success_url = reverse_lazy('course_list')
+
+class CourseUpdate(UpdateView):
+    model = LearningCourse
+    template_name = 'employee_learning/course_update.html'
+    fields = ('title', 'level', 'employee')
+    success_url = reverse_lazy('course_list')
+
+class CourseDelete(DeleteView):
+    model = LearningCourse
+    template_name = 'employee_learning/course_delete.html'
+    success_url = reverse_lazy('course_list')
