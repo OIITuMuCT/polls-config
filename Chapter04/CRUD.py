82e0f4ada16b39aa375d431ce6b758a2c142e255
OIITuMuCT <vataman@live.ru>
2024-12-16 18:49:26 +0300

    How To Write Class-Based Views with Generic Views.

--- Chapter04/CRUD.py
+++ Chapter04/CRUD.py
@@ -2,7 +2,7 @@
 # for Detail page
 # for Create page
 # for Update page
-# for Delete page 
+# for Delete page
 # add five urls patterns each view
 # add five Views using Generic Views
 # create 5 template
@@ -12,3 +12,18 @@
 # update.html
 # delite.html
 
+from django.views.generic import ListView
+from .models import MyModel
+# 1. Import generic views and models used in this view
+# 2. Create a new view using a generic view
+# 3. Set'attribute values' for each attribute defined for each generic view
+# 4. Customize the generic view(e.g., set a new context)
+
+class MyView(ListView):
+    template_name = "list.html"
+    model = MyModel
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["now"] = timezone.now()
+        return context
--- employee_learning/views.py
+++ employee_learning/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+from .models import MyModel
+
+class MyView(ListView):
+    template_name = 'list.html'
+    model = MyModel
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
